b64a7cfb47beb8ffc6d1f11820cc665010d27c65
Indu1284 <Indurevathy@gmail.com>
2021-10-18 07:30:21 +0000

    updated block.js file

---
+++ block.js
@@ -0,0 +1,13 @@
+// klasse
+class Block {
+
+        constructor(timestamp,lastHash,hash,data){
+            this.timestamp = timestamp;
+            this.lastHash = lastHash;
+            this.hash = hash;
+            this.data = data;
+        }
+
+
+
+}
